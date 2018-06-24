56e7db03588c6d5ab274b28770e3bbdbc1dea599
webstermath <webstermath@gmail.com>
2018-06-24 06:08:52 +0000

    final

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
+self.addEventListener('install',function(event){
+  event.waitUntil(
+    caches.open('restaurant-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/sw.js',
+        '/restaurant.html',
+        'js/main.js',
+        'js/dbhelper.js',
+        'js/restaurant_info.js',
+        'css/styles.css'
+        ]);
+
+    }));
+});
+
 self.addEventListener('fetch',function(event){
-    console.log(event.request)
-    
-})
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if(response) return response;
+      return fetch(event.request);
+    })
+    );
+});
